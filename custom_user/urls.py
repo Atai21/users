bb854b13807657bc62719f7f5c0776c078502946
unknown <Your email>
2021-12-18 17:42:09 +0600

    first commit

---
+++ custom_user/urls.py
@@ -0,0 +1,14 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path, include
+from . import views
+
+
+app_name = "users"
+urlpatterns = [
+    path("register/", views.RegistrationView.as_view(), name="register"),
+    path("users/", views.UserList.as_view(), name="user-list"),
+    path("login/", views.LoginUser.as_view(), name="login"),
+    path("logout/", views.logout_view),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
++ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
